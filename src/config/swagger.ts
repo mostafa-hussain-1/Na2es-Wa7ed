6c5bc04048dbccd0bd260ceb2fd52e402a644a61
Mostafa Hussain <20251700804@cis.asu.edu.eg>
2026-09-08 05:18:41 +0300

    Update swagger.ts

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -12,7 +12,8 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${process.env.PORT || 3000}`,
+        url: `https://${process.env.VERCEL_URL || 'localhost:' + (process.env.PORT || 3000)}`,
+        description: 'Server'
       },
     ],
     components: {
@@ -35,6 +36,15 @@ const options = {
 
 const swaggerSpec = swaggerJsdoc(options);
 
+const swaggerOptions = {
+  customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css',
+  customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui-standalone-preset.js'
+  ],
+  customSiteTitle: "Team Matching API Docs"
+};
+
 export const setupSwagger = (app: Express) => {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, swaggerOptions));
 };
